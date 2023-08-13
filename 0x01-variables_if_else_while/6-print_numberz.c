a16ece0549bb36ea8f00705a65de5756b1a33cba
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-13 08:47:55 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: A program that prints numbers with base10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+		putchar(x);
+	}
+
+	printf("\n");
+
+	return (0);
+}
