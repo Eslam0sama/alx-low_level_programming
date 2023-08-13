bedfd2c1b0fed21c4184d9821444fc6e69b09bc1
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-12 19:18:15 -0700

    done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: A program that prints the last digit of a random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+	for(ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for(ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	printf("\n");
+	return (0);
+}
