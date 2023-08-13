b7a5482cae69a6506246c1ba8fe5ef80e22300ea
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-13 05:21:20 -0700

    deit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: A program that prints a>z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+
+	printf("\n");
+
+	return (0);
+}
