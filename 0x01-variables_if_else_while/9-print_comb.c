bebe064d9f5e3f158e3964ffdd7ffd8e20e5c360
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-13 12:27:07 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: A program that prints the last digit of a random number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57 ; ch++)
+	{
+		putchar(ch);
+		putchar(44);
+		putchar(32);
+	}
+	putchar(10);
+	return (0);
+}
