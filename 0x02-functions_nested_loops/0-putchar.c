0952cc9d650349e2831388b4985fc65fc0243ac5
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-16 04:29:23 -0700

    done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: A program that prints numbers with base10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
