35cd753488424575181896e94c7620ffcc64f7fa
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-17 04:49:06 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - A program that prints alphabets in lower case 10  times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+	int ch;
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		putchar(10);
+	}
+
+}
