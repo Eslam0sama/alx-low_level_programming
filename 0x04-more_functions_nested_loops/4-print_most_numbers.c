51aab8a77de1d3255108d1c82f8d9f5e16cd75e7
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-17 18:55:56 -0700

    done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - A program prints numbers from 0 to 9 except 2 and 4
+ *
+ * Return:
+ */
+
+void print_most_numbers(void)
+
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i == 50 || i == 52)
+		{
+			continue;
+		}
+		_putchar(i);
+	}
+	_putchar(10);
+}
