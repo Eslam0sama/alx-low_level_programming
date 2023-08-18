290634a196625876325ae5ef09c9fd3be770de06
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-17 18:12:11 -0700

    done

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isdigit - A program that checks for upper case character
+ *
+ * @c: The character to be checked
+ *
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	if (isdigit(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
