1e67c5aaf6480418d046f006cb3de5f65f93cb40
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-17 18:49:17 -0700

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_numbers - A program that prints numbers from 0 to 9
+ *
+ * Return: return result of multiplication
+ */
+
+void print_numbers(void)
+
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
