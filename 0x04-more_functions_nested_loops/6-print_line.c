1ec64e0bc0baab5bf0214f2c17c89fb897570292
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-20 07:00:10 -0700

    done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_line - A program draws a straight line
+ *@n: the number that specify length of the line
+ * Return:
+ */
+void print_line(int n)
+{
+	int i;
+	int n;
+
+	for (i = 1; i <= n; i++)
+	{
+		putchar(_);
+	}
+	putchar(10);
+}
