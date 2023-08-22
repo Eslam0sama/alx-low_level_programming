65cebab93e15cdc7dda9dc81b9bf1a4ab6d1e83f
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-21 19:08:21 -0700

    done

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - A program reset a value to 98
+ *
+ * @n: variable
+ *
+ * @*n: pointer
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+	int *n = &n;
+	*n = 98;
+}
