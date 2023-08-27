6d5627483513c711f6672e43812999824d77b989
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-27 07:28:10 -0700

    done

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,38 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _strncpy - A program that copy a string to an array
+ *
+ * @dest: is a pointer to an array
+ *
+ * @src: is a pointer to the copied string
+ *
+ * @n: is the number of bytes
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (src[j] != '\0')
+	{
+		if (n == 0)
+			break;
+		dest[i] = src[j];
+		i++;
+		j++;
+		if (n == j)
+		{
+			j--;
+			break;
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
