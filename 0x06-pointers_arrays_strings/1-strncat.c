16b5cc9cd9d9ab6515e3a5d0b44b344f54b9fff5
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-26 20:10:56 -0700

    done

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _strncat - A program that appends string to another
+ *
+ * @dest: is a pointer to a string
+ *
+ * @src: is a pointer to the appending string
+ *
+ * @n: is the number of bytes
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, n)
+{
+	int i, j, n;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+		if (j == n - 1)
+			break;
+	}
+	return (dest);
+}
