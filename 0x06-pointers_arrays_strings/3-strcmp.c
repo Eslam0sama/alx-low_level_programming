a9112598a0d0322f72fce20b3bd0e4d7274ee263
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-08-28 06:17:51 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -20,13 +20,13 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] < s2[i])
 		{
-			result = -1;
+			result = -13;
 			break;
 		}
 
 		else if (s1[i] > s2[i])
 		{
-			result = 1;
+			result = 13;
 			break;
 		}
 		else
