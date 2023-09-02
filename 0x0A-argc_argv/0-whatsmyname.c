e64cf7978adc686fd0a6cebdd690af209e58732c
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-01 19:07:25 -0700

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - The entry point
+ *
+ * @argc: number of arguments
+ *
+ * @argv: array of the arguments
+ *
+ * Return: Always 0 for success
+*/
+int main(int argc, char *argv[])
+{
+	puts(argv[0]);
+}
