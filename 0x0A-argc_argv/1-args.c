607c816c01cbaf386d0b4ead913fc84233d15ec6
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-02 18:05:43 -0700

    edit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - The entry point
+ *
+ * @argc: number of arguments
+ *
+ * @argv: array of the arguments
+ *
+ * Return: Always 0 for success
+*/
+
+int main(int argc, char *argv[])
+{
+	_putchar(argc);
+	return 0;
+}
