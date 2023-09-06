8f861b6b3b2b2659980cdb36f50f012fb17d7c10
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-06 07:28:31 -0700

    done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - A function that counts string chars
+ *
+ * @s: is a pointer to the a string
+*/
+char* _strdup(char *str)
+{
+	int i;
+	char *x = malloc(strlen(str) + 1);
+	
+	for (i = 0; i < strlen(str) + 1; i++)
+	{
+		x[i] = str[i];
+	}
+	return x;
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Here will be tasks related to memory allocation witn malloc function
