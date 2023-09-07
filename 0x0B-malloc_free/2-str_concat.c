574a3ff7e52978c3992418a97e5e88327c64115e
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-06 19:16:25 -0700

    done

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - A function that duplicates a string
+ *
+ * @str: is a pointer to the a string
+ *
+ * Return: x
+*/
+#include <stdlib.h>
+#include <string.h>
+
+char* str_concat(char* s1, char* s2) {
+    if (s1 == NULL)
+        s1 = "";
+    if (s2 == NULL)
+        s2 = "";
+
+    size_t s1_length = strlen(s1);
+    size_t s2_length = strlen(s2);
+    size_t total_length = s1_length + s2_length;
+
+    char* result = malloc(total_length + 1);
+    if (result == NULL)
+        return NULL;
+
+    strcpy(result, s1);
+    strcat(result, s2);
+
+    return result;
+}
