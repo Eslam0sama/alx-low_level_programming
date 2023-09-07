7b5db3f1d74eed14415ccf5660246bcab7b59f7c
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-06 19:00:47 -0700

    done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,27 +4,29 @@
 #include <string.h>
 
 /**
- * _strdup - A function that counts string chars
+ * _strdup - A function that duplicates a string
  *
  * @str: is a pointer to the a string
+ *
+ * Return: x
 */
-char* _strdup(char *str)
+char *_strdup(char *str)
 {
 	int i;
 	char *x;
 
 	if (str == NULL)
-		return NULL;
+		return (NULL);
 
-	x = malloc((strlen(str) + 1) * sizeof (char));
+	x = malloc((strlen(str) + 1) * sizeof(char));
 
 	if (x == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	for (i = 0; i < (int)(strlen(str) + 1); i++)
 	{
 		x[i] = str[i];
 	}
-		
-	return x;
+
+	return (x);
 }
