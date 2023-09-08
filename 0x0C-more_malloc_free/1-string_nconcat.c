76df446efabf58b8810161e77858d9725d273524
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-07 19:32:12 -0700

    done

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * malloc_checked - A function that duplicates a string
+ *
+ * @ptr: is a pointer to a memory
+ *
+ * Return: ptr
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return ptr;
+	free(ptr);
+}
