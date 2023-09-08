deaa216b5167a0a82dff97561b687083cdc3e7ba
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-07 19:04:21 -0700

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - A function that duplicates a string
+ *
+ * @str: is a pointer to the a string
+ *
+ * Return: x
+*/
+void *malloc_checked(unsigned int b)
+{
+	b = malloc(sizeof(int));
+	if (b == NULL)
+	{
+		return 98;
+	}
+}
