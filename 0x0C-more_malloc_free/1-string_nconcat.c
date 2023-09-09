155ae9c8a4381d90f0713f61fbad109defeca32a
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-08 19:37:32 -0700

    edit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,10 +35,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		ptr[i] = s1[i];
 	}
+
+	if (n >= (strlen(s2)))
+	{
+		n = strlen(s2); 
+	}
+
 	for (j = 0; j < n; j++, i++)
 	{
+
 		ptr[i] = s2[j];
 	}
+
 	ptr[i] = '\0';
 
 	return (ptr);
