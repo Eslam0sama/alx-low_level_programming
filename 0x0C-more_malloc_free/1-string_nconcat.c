5302984d5590f95676893186c10afd7875ea43ed
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-08 18:59:14 -0700

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,8 +19,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr = (char*)malloc(strlen(s1) + n + 1);
 
 	if (ptr == NULL)
-	return (NULL);
-
+	{
+		return (NULL);
+	}
 	for (i = 0; i < strlen(s1); i++)
 	{
 		ptr[i] = s1[i];
