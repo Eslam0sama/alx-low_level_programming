e98089f2af62ba4259d9bbda41f7ec05269e5865
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-11 06:24:57 -0700

    done

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include <ctype.h>
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - A function that allocates memory with number of elemnts
+ *
+ * @nmemb: number of elemnts
+ *
+ * @size: number of bytes each element needs
+ *
+ * Return: ptr
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    void *ptr;
+    unsigned int i;
+    int *cptr;
+
+    if (nmemb == 0 || size == 0)
+    {
+        return NULL;
+    }
+
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL)
+        return NULL;
+
+    cptr = (int *)ptr;
+
+    for (i = 0; i < (nmemb * size); i++)
+    {
+        cptr[i] = 0;
+    }
+    return ptr;
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -30,4 +30,5 @@ int _strlen_recursion(char *s);
 char *_strdup(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
