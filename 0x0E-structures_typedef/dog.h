82a701457b4ccff438d346673ef49e98c203bcfc
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-12 06:28:28 -0700

    done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
-struct dog 
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog's basic info
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: name of the owner of dog
+ */
+
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
