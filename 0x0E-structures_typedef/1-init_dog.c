2afb9fc123ce170ec2fcb3abe57a2c5820b074c2
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-12 19:16:34 -0700

    done

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,13 @@
 #include "dog.h"
  
 /**
+ * init_dog - intializes a dog
+ * @d:
+ * @name:
+ * @age:
+ * @owner:
+ *
+ * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
