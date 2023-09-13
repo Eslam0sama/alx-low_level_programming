5902c8255c5304ecb43ec4be8e72c91d5ee82b93
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-13 06:13:35 -0700

    done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - intializes a dog
+ * @d: the dog to init
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	printf("Name: %s\nAge: %lf\nOwner: %s", d->name, d->age, d->owner);
+}
