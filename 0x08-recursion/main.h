129b40eda9558f581a959775ab09bdb572fe5c7d
Eslam0sama <eslam.osama.abdullah@gmail.com>
2023-09-24 17:33:32 -0700

    done

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -27,4 +27,6 @@ int _strcmp(char *s1, char *s2);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
