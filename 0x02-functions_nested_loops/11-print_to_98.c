3d258a3338ae4048a6dea5a36d75a446f4172e25
Eslam0sama <eslam.abdulla08@gmail.com>
2024-03-06 17:37:06 -0800

    Done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,40 +8,33 @@
 
 void print_to_98(int n)
 {
-	while (n == 98)
-	{
-		printf("%d", n);
-		break;
-	}
-
-	while (n < 98)
+	while (n <= 98 || n >= 98)
 	{
 		if (n == 98)
 		{
 			printf("%d", n);
 			break;
 		}
-
-		else
+		else if (n <= 98)
 		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				break;
+			}
 			printf("%d, ", n);
 			n++;
 		}
-
-	}
-	while (n > 98)
-	{
-		if (n == 98)
-		{
-			printf("%d", n);
-			break;
-		}
 		else
 		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				break;
+			}
 			printf("%d, ", n);
 			n--;
 		}
-
 	}
 	printf("\n");
 }
