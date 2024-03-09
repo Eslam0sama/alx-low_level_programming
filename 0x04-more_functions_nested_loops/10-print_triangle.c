f8f39a2a6a87f44a82131f8d5c81ff421b830ab9
Eslam0sama <eslam.abdulla08@gmail.com>
2024-03-08 17:19:00 -0800

    Done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+/**
+ * print_triangle - A function that print a triangle of a specific size.
+ *
+ * @size: The size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int row, spaces, hashes;
+
+	if (size == 0)
+	{
+		_putchar('\n');
+		break;
+	}
+	for (row = 0; row < size; row++)
+	{
+		for (spaces = row; spaces < size - 1; spaces++)
+		{
+			_putchar(' ');
+		}
+		for (hashes = 0; hashes <= row; hashes++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
