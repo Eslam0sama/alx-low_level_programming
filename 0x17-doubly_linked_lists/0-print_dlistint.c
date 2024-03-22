5e978e74d8bf416629b321a192c94010c2a739e7
Eslam0sama <eslam.abdulla08@gmail.com>
2024-03-21 19:47:39 -0700

    done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,28 +1,33 @@
-#include "lists.h"
+#ifndef LISTS
+#define LISTS
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
- * @h: pointer to the list.
- * Return: number of nodes.
- **/
-
-size_t print_dlistint(const dlistint_t *h)
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
 {
-    size_t count = 0; // Counter to keep track of the number of elements printed
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 
-    if (h == NULL)
-    {
-        return 0; // Return 0 if the list is empty
-    }
-    else
-    {
-        while (h != NULL)
-        {
-            printf("%d ", h->n); // Print the current node's value
-            count++; // Increment the counter
-            h = h->next; // Move to the next node
-        }
-        printf("\n"); // Print a newline after printing all elements
-    }
-    return count; // Return the number of elements printed
-}
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+void free_dlistint(dlistint_t *head);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+
+#endif
