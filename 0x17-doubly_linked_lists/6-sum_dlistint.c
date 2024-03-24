93333ea5eef5c89b2f36ef78da72d797b0f817e0
Eslam0sama <eslam.abdulla08@gmail.com>
2024-03-23 17:04:49 -0700

    Done

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+/**
+ * sum_dlistint - A function to sum all numbers in a dlist
+ *
+ * @head: The head of the list
+ *
+ * Return: The sum of the list's data
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	int sum = 0;
+
+	current = head;
+
+	while (current)
+	{
+		sum += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
